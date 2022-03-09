a5c1f73e271fbc9f244408fb74495c0f2678ad1c
ammar <ammarraneez@gmail.com>
2022-03-09 10:42:45 +0530

    Add concat plugin configs

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,18 +4,24 @@
 module.exports = function (grunt) {
   // config
   grunt.initConfig({
-
+    concat: {
+      js: {
+        // src: ['js/jquery-1.12.4.js', 'js/rslides.js', 'js/scripts.js'],    // specific files
+        src: ['js/*.js'],                                                     // all js files in js directory
+        dest: 'build/scripts.js'
+      },
+      css: {
+        src: ['css/*.css'],
+        dest: 'build/styles.css' 
+      }
+    }
   });
 
   // load plugins
-  grunt.loadNpmTasks('');
+  grunt.loadNpmTasks('grunt-contrib-concat');
 
+  // grunt concat will run all within grunt concat init config (this is due to the plugin)
   // Register tasks
-  grunt.registerTask('run', function () {
-    console.log('I am running');
-  });
-  grunt.registerTask('sleep', function () {
-    console.log('I am sleeping');
-  });
-  grunt.registerTask('all', ['sleep', 'run']);
+  grunt.registerTask('concat-js', ['concat:js']);
+  grunt.registerTask('concat-css', ['concat:css']);
 }
